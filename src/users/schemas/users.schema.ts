9397c09c5b7f6fc205822fd5a9417459fd1b0594
danrleydimas <danrley-dimas@hotmail.com>
2024-11-12 17:06:59 -0300

    configuration slint

--- src/users/schemas/users.schema.ts
+++ src/users/schemas/users.schema.ts
@@ -1,26 +1,26 @@
 import * as mongoose from 'mongoose'
 import * as bcrypt from 'bcrypt'
 
-export const UsersSchema =  new mongoose.Schema({
-    name:{
-        type:String,
+export const UsersSchema = new mongoose.Schema({
+    name: {
+        type: String,
     },
-    email:{
-        type:String
+    email: {
+        type: String
     },
-    password:{
-        type:String
-    }
+    password: {
+        type: String
+    },
 });
 UsersSchema.pre('save', async function (next) {
-    try{
-        if(!this.isModified('password')) {
+    try {
+        if (!this.isModified('password')) {
             return next()
         }
 
-        this['password'] = await bcrypt.hash( this['password'],10)
-    } catch(err){
+        this['password'] = await bcrypt.hash(this['password'], 10)
+    } catch (err) {
         return next(err);
     }
-    
+
 })
